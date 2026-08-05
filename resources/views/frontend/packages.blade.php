b85dcf327c11a7c3ff4b4c17c75dba3dd81adc29
umer-two-bit <umeralampklhr@gmail.com>
2026-08-06 02:25:19 +0500

    fix monthly/yearly billing toggle with segmented pill UI

--- resources/views/frontend/packages.blade.php
+++ resources/views/frontend/packages.blade.php
@@ -4,56 +4,6 @@
 
 @section('content')
     <style>
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-        }
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-        input:checked + .slider {
-            background-color: #2196F3;
-        }
-        input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
-        }
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-        }
-        .slider.round {
-            border-radius: 34px;
-        }
-        .slider.round:before {
-            border-radius: 50%;
-        }
         .yearly_price {
             display: none;
         }
@@ -78,12 +28,7 @@
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="text-center">
-                        <span class="beforeinput text-success">Monthly</span>
-                        <label class="switch">
-                            <input id="toggle-event" type="checkbox" data-toggle="toggle">
-                            <span class="slider round"></span>
-                        </label>
-                        <span class="afterinput">Yearly</span>
+                        @include('partials.billing_toggle')
                     </div>
                 </div>
 
@@ -153,19 +98,27 @@
     </main>
 
     @include('partials.script_file')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script>
-        $(document).ready(function() {
-            $(".monthly_price").show();
-            $(".yearly_price").hide();
-            $('input[type="checkbox"]').click(function() {
-                if($(this).prop("checked") == true) {
-                    $(".monthly_price").hide();
-                    $(".yearly_price").show();
-                } else {
-                    $(".monthly_price").show();
-                    $(".yearly_price").hide();
-                }
+        document.querySelectorAll('.billing-toggle').forEach(function (toggle) {
+            toggle.addEventListener('click', function (e) {
+                var btn = e.target.closest('.billing-toggle__btn');
+                if (!btn) return;
+
+                var isYearly = btn.getAttribute('data-billing') === 'yearly';
+
+                toggle.querySelectorAll('.billing-toggle__btn').forEach(function (b) {
+                    b.classList.toggle('active', b === btn);
+                });
+
+                var scope = toggle.closest('.wt-section') || document;
+
+                scope.querySelectorAll('div.monthly_price').forEach(function (el) {
+                    el.style.display = isYearly ? 'none' : 'block';
+                });
+
+                scope.querySelectorAll('div.yearly_price').forEach(function (el) {
+                    el.style.display = isYearly ? 'block' : 'none';
+                });
             });
         });
     </script>
