2a458d583a1bcd3c2b09cd429bde6258a6f5ef3a
umer-two-bit <umeralampklhr@gmail.com>
2026-08-05 19:31:43 +0500

    import homepage views and assets from home_page_design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@500;600;700;800&family=Source+Sans+3:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @stack('styles')
+
+    {{-- Scripts that must load at the start (e.g. analytics/conversion snippets) --}}
+    @stack('scripts-top')
 </head>
 <body>
     <x-header />
